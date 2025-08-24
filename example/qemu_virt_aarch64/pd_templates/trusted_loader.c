3c2ddbbba9fa4f11b52511fcd43d31d314350011
Zhu Guangtao <guangtaozhu@outlook.com>
2025-08-24 23:23:37 +1000

    fix trusted loader
    Signed-off-by: Zhu Guangtao <guangtaozhu@outlook.com>

--- example/qemu_virt_aarch64/pd_templates/trusted_loader.c
+++ example/qemu_virt_aarch64/pd_templates/trusted_loader.c
@@ -13,25 +13,7 @@
 
 #include <microkit.h>
 
-#ifndef ED25519_H
-#define ED25519_H
-#include <stddef.h>
-
-#if defined(_WIN32)
-    #if defined(ED25519_BUILD_DLL)
-        #define ED25519_DECLSPEC __declspec(dllexport)
-    #elif defined(ED25519_DLL)
-        #define ED25519_DECLSPEC __declspec(dllimport)
-    #else
-        #define ED25519_DECLSPEC
-    #endif
-#else
-    #define ED25519_DECLSPEC
-#endif
-
-int ED25519_DECLSPEC ed25519_verify(const unsigned char *signature, const unsigned char *message, size_t message_len, const unsigned char *public_key);
-#endif
-
+#include "ed25519.h"
 #include "elf.h"
 #include "elf_utils.h"
 
