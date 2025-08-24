647589e0afb82ec03cb731711568bba2da23c369
Zhu Guangtao <guangtaozhu@outlook.com>
2025-08-24 18:59:35 +1000

    use libcrypto in trusted loader
    Signed-off-by: Zhu Guangtao <guangtaozhu@outlook.com>

--- example/qemu_virt_aarch64/pd_templates/trusted_loader.c
+++ example/qemu_virt_aarch64/pd_templates/trusted_loader.c
@@ -13,7 +13,24 @@
 
 #include <microkit.h>
 
-#include "ed25519.h"
+#ifndef ED25519_H
+#define ED25519_H
+#include <stddef.h>
+
+#if defined(_WIN32)
+    #if defined(ED25519_BUILD_DLL)
+        #define ED25519_DECLSPEC __declspec(dllexport)
+    #elif defined(ED25519_DLL)
+        #define ED25519_DECLSPEC __declspec(dllimport)
+    #else
+        #define ED25519_DECLSPEC
+    #endif
+#else
+    #define ED25519_DECLSPEC
+#endif
+
+int ED25519_DECLSPEC ed25519_verify(const unsigned char *signature, const unsigned char *message, size_t message_len, const unsigned char *public_key);
+#endif
 
 #include "elf.h"
 #include "elf_utils.h"
