a0bb1023549bd0b6122c2951eaead575ebeaedcf
Zhu Guangtao <guangtaozhu@outlook.com>
2025-09-02 20:06:09 +1000

    make another container monitor work
    Signed-off-by: Zhu Guangtao <guangtaozhu@outlook.com>

--- example/qemu_virt_aarch64/pd_templates/sender_loader.c
+++ example/qemu_virt_aarch64/pd_templates/sender_loader.c
@@ -8,18 +8,17 @@
 uintptr_t shared1;
 uintptr_t shared2;
 
-extern char _sender[];
-extern char _sender_end[];
+extern char _container[];
+extern char _container_end[];
 
 extern char _client[];
 extern char _client_end[];
 
 void init(void)
 {
-    while (1);
     microkit_dbg_printf(PROGNAME "Entered init\n");
 
-    custom_memcpy((void *)shared1, _sender, _sender_end - _sender);
+    custom_memcpy((void *)shared1, _container, _container_end - _container);
     microkit_dbg_printf(PROGNAME "Wrote sender's ELF file into memory\n");
     custom_memcpy((void *)shared2, _client, _client_end - _client);
     microkit_dbg_printf(PROGNAME "Wrote client's ELF file into memory\n");
@@ -29,7 +28,8 @@ void init(void)
     microkit_msginfo info;
     seL4_Error error;
     
-    info = microkit_ppcall(1, microkit_msginfo_new(0, 0));
+    microkit_mr_set(0, 1);
+    info = microkit_ppcall(1, microkit_msginfo_new(0, 1));
     error = microkit_msginfo_get_label(info);
     if (error != seL4_NoError) {
         microkit_internal_crash(error);
