274f75ca8703948c6e1aa0f682c78efab18ee61d
Zhu Guangtao <guangtaozhu@outlook.com>
2025-12-26 18:46:49 +1100

    Proto container
    Signed-off-by: Zhu Guangtao <guangtaozhu@outlook.com>

--- example/pd_dyn/template.c
+++ example/pd_dyn/template.c
@@ -17,6 +17,8 @@ uint64_t image_client_payload   = 0x50000000;
 uint64_t image_trampoline       = 0x30000000;
 uint64_t image_trusted_loader   = 0x10000000;
 
+#define ELF_FILE_SIZE           0x800000
+
 // Where to get the image of trusted loader
 extern char _loader[];
 extern char _loader_end[];
@@ -27,7 +29,16 @@ extern char _payload_end[];
 extern char _trampoline[];
 extern char _trampoline_end[];
 
+// A shared memory region with container, containing content from tsldr_metadata_patched
+// Will be init each time the container restarts by copying the data from above
+uintptr_t tsldr_metadata;
+// base of all shared metadata regions
+tsldr_md_t *tsldr_metadata_base = (tsldr_md_t *)0xffc0000;
+
+#define PROGNAME "[@monitor] "
+
 void microkit_dbg_printf(const char *format, ...);
+
 
 static inline void serial_putc(char ch)
 {
@@ -47,34 +58,31 @@ static inline void serial_puts(const char *s)
 
 void init(void)
 {
-    //microkit_dbg_puts("hello, world. my name is ");
-    microkit_dbg_puts(microkit_name);
-    microkit_dbg_puts("\n");
-
-    microkit_dbg_puts("Now writing to serial I/O port: ");
     serial_puts("hello!\n");
-
-    // test capDL spec generation and capability distribution...
-    seL4_Signal(425);
-    seL4_Signal(441);
-
-    seL4_Error err = seL4_CNode_Copy(
-        425,    // target CNode
-        16,     // target slot
-        64,     // PD_CAP_BITS
-        441,    // source CNode
-        16,     // source slot
-        64,     // PD_CAP_BITS
-        seL4_AllRights
-    );
-    if (err != seL4_NoError) {
-        microkit_dbg_printf("Failed to copy channel cap from BGD to working CNode\n");
-        //microkit_internal_crash(err);
-    }
 
     tsldr_md_array_t *ptr_spec_trusted_loader = (tsldr_md_array_t *)microkit_template_spec;
     microkit_dbg_printf("%d\n", ptr_spec_trusted_loader->avails);
     microkit_dbg_printf("%s\n", microkit_name);
+
+    int num_assert = 0;
+    for (int i = 0; i < 16; ++i) {
+        // must provide valid hash to 
+        if (ptr_spec_trusted_loader->md_array[i].system_hash != 0xffff) {
+            // do not initialise unspecified tsldr metadata
+            continue;
+        }
+        num_assert += 1;
+        // adjust global pointer
+        tsldr_metadata = (uintptr_t)((char *)tsldr_metadata_base + i * 0x1000);
+        microkit_dbg_printf(PROGNAME "tsldr_metadata: 0x%x\n", tsldr_metadata);
+        // initialise the target tsldr_metadata
+        tsldr_init_metadata(ptr_spec_trusted_loader, i);
+    }
+    if (num_assert != ptr_spec_trusted_loader->avails) {
+        microkit_dbg_printf(PROGNAME "avail num: %d - num assert: %d\n", ptr_spec_trusted_loader->avails, num_assert);
+        microkit_internal_crash(-1);
+    }
+    microkit_dbg_printf(PROGNAME "finished template PD initialization\n");
 }
 
 void notified(microkit_channel ch)
@@ -97,14 +105,26 @@ seL4_Bool fault(microkit_child child, microkit_msginfo msginfo, microkit_msginfo
         // You can use microkit_pd_restart to restart the child PD...
         if (ip == 0x0) {
             microkit_pd_stop(child);
-            microkit_dbg_printf("Restart faulting PD at the entry of trusted loaders...\n");
+            microkit_dbg_printf(PROGNAME "restart faulting PD at the entry of trusted loaders...\n");
 
             Elf64_Ehdr *ehdr = (Elf64_Ehdr *)_loader;
             if (custom_memcmp(ehdr->e_ident, (const unsigned char*)ELFMAG, SELFMAG) != 0) {
                 microkit_dbg_printf("Data in shared memory region must be an ELF file\n");
                 return seL4_False;
             }
+            microkit_dbg_printf(PROGNAME "trying to load trusted_loader from: %x\n", _loader);
+            microkit_dbg_printf(PROGNAME "trying to load payload from: %x\n", _payload);
+            microkit_dbg_printf(PROGNAME "trying to load trampoline from: %x\n", _trampoline);
+
             load_elf((void*)image_trusted_loader, ehdr);
+            microkit_dbg_printf(PROGNAME "loaded trusted loader successfully\n");
+
+            custom_memcpy((void*)((uintptr_t)image_client_payload), (char*)_payload, _payload_end - _payload);
+            microkit_dbg_printf(PROGNAME "loaded client payload successfully\n");
+
+            custom_memcpy((void*)((uintptr_t)image_trampoline), (char*)_trampoline, _trampoline_end - _trampoline);
+            microkit_dbg_printf(PROGNAME "loaded trampoline payload successfully\n");
+
             // FIXME: do sanity checks when the dynamic PD faults on 0x0
             microkit_pd_restart(child, ehdr->e_entry);
             return seL4_False;
@@ -115,130 +135,3 @@ seL4_Bool fault(microkit_child child, microkit_msginfo msginfo, microkit_msginfo
     // Stop the thread explicitly; no need to reply to the fault
     return seL4_False;
 }
-
-
-/**
- * @brief Outputs an unsigned 64-bit integer in decimal format.
- *
- * @param val The unsigned 64-bit integer to print.
- */
-void putdec(uint64_t val) {
-    // Buffer to hold the decimal digits (max 20 digits for uint64_t)
-    char buffer[21];
-    int index = 20; // Start from the end of the buffer
-    buffer[index] = '\0'; // Null-terminate the string
-
-    if (val == 0) {
-        buffer[--index] = '0';
-    } else {
-        while (val > 0 && index > 0) {
-            buffer[--index] = '0' + (val % 10);
-            val /= 10;
-        }
-    }
-
-    // Output the resulting string
-    const char *str = &buffer[index];
-    while (*str) {
-        microkit_dbg_putc(*str++);
-    }
-}
-
-/**
- * @brief Outputs an unsigned 64-bit integer in hexadecimal format.
- *
- * @param val The unsigned 64-bit integer to print.
- */
-void puthex(uint64_t val) {
-    // Prefix for hexadecimal representation
-    microkit_dbg_puts("0x");
-
-    // Buffer to hold the hexadecimal digits (max 16 digits for uint64_t)
-    char buffer[17];
-    int index = 16; // Start from the end of the buffer
-    buffer[index] = '\0'; // Null-terminate the string
-
-    if (val == 0) {
-        buffer[--index] = '0';
-    } else {
-        while (val > 0 && index > 0) {
-            uint8_t digit = val & 0xF; // Get the last 4 bits
-            if (digit < 10) {
-                buffer[--index] = '0' + digit;
-            } else {
-                buffer[--index] = 'a' + (digit - 10);
-            }
-            val >>= 4; // Shift right by 4 bits to process the next digit
-        }
-    }
-
-    // Output the resulting string
-    const char *str = &buffer[index];
-    while (*str) {
-        microkit_dbg_putc(*str++);
-    }
-}
-
-void microkit_dbg_printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
-
-    const char *ptr = format;
-
-    while (*ptr != '\0') {
-        if (*ptr == '%') {
-            ptr++; // Move past '%'
-
-            switch (*ptr) {
-                case 's': {
-                    // String
-                    const char *str = va_arg(args, const char *);
-                    if (str != 0) {
-                        microkit_dbg_puts(str);
-                    } else {
-                        microkit_dbg_puts("(null)");
-                    }
-                    break;
-                }
-                case 'd': {
-                    // Decimal
-                    uint64_t val = va_arg(args, uint64_t);
-                    putdec(val);
-                    break;
-                }
-                case 'x': {
-                    // Hexadecimal
-                    uint64_t val = va_arg(args, uint64_t);
-                    puthex(val);
-                    break;
-                }
-                case 'c': {
-                    // Character
-                    int c = va_arg(args, int); // char is promoted to int
-                    microkit_dbg_putc((char)c);
-                    break;
-                }
-                case '%': {
-                    // Literal '%'
-                    microkit_dbg_putc('%');
-                    break;
-                }
-                default: {
-                    // Unsupported format specifier, print it literally
-                    microkit_dbg_putc('%');
-                    microkit_dbg_putc(*ptr);
-                    break;
-                }
-            }
-            ptr++; // Move past format specifier
-        } else {
-            // Regular character
-            microkit_dbg_putc(*ptr);
-            ptr++;
-        }
-    }
-
-    va_end(args);
-}
-
-
---
+++ example/pd_dyn/trampoline.c
@@ -0,0 +1,84 @@
+#include <microkit.h>
+#include <stdint.h>
+#include <elfutils.h>
+
+typedef void (*entry_fn_t)(void);
+
+__attribute__((noreturn, naked))
+static void jump_with_stack(void *new_stack, void (*entry)(void))
+{
+    __asm__ volatile(
+        "mov %rdi, %rsp\n\t"   /* new_stack in rdi */
+        "jmp *%rsi\n\t"        /* entry in rsi */
+    );
+}
+
+
+void init(void)
+{
+    uintptr_t acgroup_metadata  = 0xA01000;
+    uintptr_t tsldr_metadata    = 0xA00000;
+    uintptr_t tsldr_program     = 0x200000;
+    uintptr_t tsldr_context     = 0xE00000;
+    uintptr_t tsldr_stack_bottom        = 0x0FFFFFFF000;
+    uintptr_t container_stack_bottom    = 0x00FFFBFF000;
+    uintptr_t container_stack_top       = 0x00FFFC00000;
+    uintptr_t client_elf = 0x2000000;
+
+    microkit_dbg_puts("[@trampoline] Entry of trampoline.\n");
+
+    /* say goodbye to the old stack */
+    custom_memset((void *)tsldr_stack_bottom, 0, 0x1000);
+
+    /* clean up trusted loader metadata... */
+    custom_memset((void *)tsldr_metadata, 0, 0x1000);
+
+    /* clean up access rights group metadata */
+    // is disposable...
+    custom_memset((void *)acgroup_metadata, 0, 0x1000);
+
+    /* clean up trusted loader... */
+    custom_memset((void *)tsldr_program, 0, 0x800000);
+
+    /* clean up container stack... */
+    custom_memset((void *)container_stack_bottom, 0, 0x1000);
+
+    // syscall for tsldr_context cleanup
+    microkit_mr_set(0, 20);
+    // try to call the monitor to backup trusted loading context
+    microkit_msginfo info = microkit_ppcall(15, microkit_msginfo_new(0, 1));
+    seL4_Error error = microkit_msginfo_get_label(info);
+    if (error != seL4_NoError) {
+        microkit_internal_crash(error);
+    }
+    // clean up trusted loading context...
+    custom_memset((void *)tsldr_context, 0, 0x1000);
+
+    /* at this point we dont have access to the data section of tsldr */
+    microkit_dbg_puts("[@trampoline] Exit of trampoline.\n");
+
+    /*
+     * At this point, the client information is embedded in the address space,
+     * while the trusted loader and all older stacks are gone for good...
+     * It's fine to just jump to the new stack/function for the real container
+     */
+    Elf64_Ehdr *ehdr = (Elf64_Ehdr *)client_elf;
+    entry_fn_t entry_fn = (entry_fn_t) ehdr->e_entry;
+#if 0
+    asm volatile (
+        "mov sp, %[new_stack]\n\t" /* set new SP */
+        "br  %[func]\n\t"          /* branch directly, never return */
+        :
+        : [new_stack] "r" (container_stack_top),
+          [func] "r" (entry_fn)
+        : "x30", "memory"
+    );
+#endif
+    jump_with_stack((void *)container_stack_top, entry_fn);
+    __builtin_unreachable();
+}
+
+void notified(microkit_channel ch)
+{
+    ;
+}
